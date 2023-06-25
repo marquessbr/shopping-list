a9b591474eae926b7b4d1e04f179f55205e22333
Armando M S Sobrinho <marquessbr@gmail.com>
2023-06-25 08:45:35 -0300

    inicio implement routers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  {path: '', redirectTo: '/recipes'},
+  {path: 'recipes', component: RecipesComponent},
+  {path: 'recipes', component: RecipesComponent},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
